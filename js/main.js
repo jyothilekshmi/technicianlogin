5ee1c1f5457e02c1799f9e2283d8862e836b2d0d
jyothi-lekshmi <Jyothilekshmi93@gmail.com>
2017-01-10 11:15:27 +0530

    jo

--- js/main.js
+++ js/main.js
@@ -101,5 +101,12 @@ $(function()
         });
   
   });
-  
->>>>>>> origin/master
+
+  $(document).ready(function(){
+    $('.accordion-toggle').on('click', function(){
+        $(this).closest('.panel-group').children().each(function(){
+            $(this).find('>.panel-heading').removeClass('active');
+        });
+        $(this).closest('.panel-heading').toggleClass('active');
+    });
+});
